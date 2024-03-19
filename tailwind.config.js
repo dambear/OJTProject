198bd3e393c2451803bdfb40d29d08266a3c3fc6
KirtSywin <f.kirtsywin4@gmail.com>
2024-03-19 10:43:19 +0800

    replenishing styles of dashboards and tables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,29 @@ module.exports = {
   content: ["./templates/**/*.html", "./node_modules/flowbite/**/*.js"],
   // make sure to safelist these classes when using purge
   safelist: [
-    "w-64",
-    "w-1/2",
-    "rounded-l-lg",
-    "rounded-r-lg",
-    "bg-gray-200",
-    "grid-cols-4",
-    "grid-cols-7",
-    "h-6",
-    "leading-6",
-    "h-9",
-    "leading-9",
-    "shadow-lg",
+     "w-64",
+     "w-1/2",
+     "rounded-l-lg",
+     "rounded-r-lg",
+     "bg-gray-200",
+     "grid-cols-4",
+     "grid-cols-7",
+     "h-6",
+     "leading-6",
+     "h-9",
+     "leading-9",
+     "shadow-lg",
   ],
-
-  // enable dark mode via class strategy
-  darkMode: "class",
-
+ 
+  // Disable dark mode
+  darkMode: false,
+ 
   theme: {
-    extend: {},
+     extend: {},
   },
   plugins: [
-    require("flowbite/plugin")({
-      charts: true,
-    }),
+     require("flowbite/plugin")({
+       charts: true,
+     }),
   ],
-};
+ };
